edeba895485d284c530108f0f15ddc527652bce6
ayeshactl <kaziayesha009@gmail.com>
2026-04-15 13:20:49 +0530

    final todo app

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Mongo Connect
+mongoose.connect(process.env.MONGO_URL);
+// Schema
+const Todo = mongoose.model("Todo", {
+  text: String,
+  completed: { type: Boolean, default: false }
+});
+
+// GET all todos
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+// ADD todo
+app.post("/add", async (req, res) => {
+  const todo = new Todo({ text: req.body.text });
+  await todo.save();
+  res.json(todo);
+});
+
+// DELETE todo
+app.delete("/delete/:id", async (req, res) => {
+  await Todo.findByIdAndDelete(req.params.id);
+  res.json({ message: "Deleted" });
+});
+
+// TOGGLE complete
+app.put("/toggle/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+  todo.completed = !todo.completed;
+  await todo.save();
+  res.json(todo);
+});
+
+app.listen(3000, () => {
+  console.log("Backend running");
+});
